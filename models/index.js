f1f2bf1a7a2de14d33ea48d6272e6cec00422103
Sergio Torres <sergio.lo.torres@gmail.com>
2021-05-12 22:31:21 -0500

    fixed typo at associations

--- models/index.js
+++ models/index.js
@@ -11,7 +11,7 @@ Product.belongsTo(Category,{
 })
 
 // Categories have many Products
-Category.hasMany(Products,{
+Category.hasMany(Product,{
   foreignKey:'categeory_id'
 })
 // Products belongToMany Tags (through ProductTag)
